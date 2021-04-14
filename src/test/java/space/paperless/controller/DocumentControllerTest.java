bd0c234206af654c59993556cdc31c098d83948e
vince <vincent@reviol.space>
2021-04-14 21:51:39 +0200

    fix: backup unknown fixes before backup

--- src/test/java/space/paperless/controller/DocumentControllerTest.java
+++ src/test/java/space/paperless/controller/DocumentControllerTest.java
@@ -1,44 +1,36 @@
 package space.paperless.controller;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.accept.ContentNegotiationManager;
-import org.springframework.web.accept.FixedContentNegotiationStrategy;
 
-import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
 import space.paperless.repository.IdUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @TestPropertySource("/test-application.properties")
+@AutoConfigureMockMvc
 public class DocumentControllerTest {
+	private static final String ID = IdUtils.id("archive", "type2", "201501_Captain_Future.pdf");
+	private static final String REPOSITORIES_ARCHIVE_DOCUMENTS = "/repositories/archive/documents/";
 
 	@Autowired
-	private DocumentController controller;
+	private MockMvc mvc;
 
 	@Test
-	public void get_document_documentIsReturned() {
-		MockMvc mvc = MockMvcBuilders.standaloneSetup(controller).setContentNegotiationManager(
-				new ContentNegotiationManager(new FixedContentNegotiationStrategy(MediaType.APPLICATION_JSON_UTF8)))
-				.build();
-		ValidatableMockMvcResponse response = given().mockMvc(mvc).when()
-				.get("/repositories/archive/documents/" + IdUtils.id("archive", "type2", "201501_Captain_Future.pdf"))
-				.then();
-
-		response.statusCode(200);
-		response.body("documentId", equalTo(IdUtils.id("archive", "type2", "201501_Captain_Future.pdf")));
-		response.body("descriptions.name", hasItem(IdUtils.id("Captain_Future.pdf")));
+	public void get_document_documentIsReturned() throws Exception {
+		mvc.perform(get(REPOSITORIES_ARCHIVE_DOCUMENTS + ID)).andExpect(status().isOk())
+				.andExpect(jsonPath("$.documentId", is(ID)));
 	}
 }
--- src/test/java/space/paperless/controller/FilesControllerTest.java
+++ src/test/java/space/paperless/controller/FilesControllerTest.java
@@ -1,33 +1,32 @@
 package space.paperless.controller;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
 import space.paperless.repository.IdUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @TestPropertySource("/test-application.properties")
+@AutoConfigureMockMvc
 public class FilesControllerTest {
-
 	@Autowired
-	private FilesController controller;
+	private MockMvc mvc;
 
 	@Test
-	public void get_file_fileIsReturned() {
-		ValidatableMockMvcResponse response = given().standaloneSetup(controller).when()
-				.get("/repositories/archive/files/" + IdUtils.id("archive", "type2", "201501_Captain_Future.pdf"))
-				.then();
-
-		response.statusCode(200);
-		response.contentType("application/pdf");
+	public void get_file_fileIsReturned() throws Exception {
+		mvc.perform(get("/repositories/archive/files/" + IdUtils.id("archive", "type2", "201501_Captain_Future.pdf")))
+				.andExpect(status().isOk()).andExpect(content().contentType("application/pdf"));
 	}
 }
